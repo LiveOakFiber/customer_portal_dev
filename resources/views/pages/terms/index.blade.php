0e0b7f0421ab56bbcf701f5d8f397ba6b99cd8a6
John Sutherland <john.sutherland@liveoakfiber.com>
2023-09-27 16:33:21 -0400

    Added a page for Terms and Conditions

---
+++ resources/views/pages/terms/index.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.full')
+@section('content')
+<div class="container-fluid">
+      <div class="row justify-content-center">
+         <div class="col-12">
+            <!-- Header -->
+            <div class="header mt-md-5">
+               <div class="header-body">
+                  <div class="row align-items-center">
+                     <div class="col">
+                        <!-- Pretitle -->
+                        <h6 class="header-pretitle">
+                           <!--{{utrans("headers.summary")}}-->
+                           
+                        </h6>
+                        <!-- Title -->
+                        <h1 class="header-title">
+                           <!--{{utrans("headers.contracts")}}-->
+                           Terms and Conditions
+                        </h1>
+                     </div>
+                     <div class="col-auto">
+                     </div>
+                  </div>
+                  <!-- / .row -->
+               </div>
+            </div>
+        </div>
+    </div>
+</div>
+            
